d33fa2815a540b30e80d90cdada4e6b94c596e5f
parthmp <parthparmaronline@gmail.com>
2025-11-10 08:47:21 +0530

    color changes

--- app/pages/resume.vue
+++ app/pages/resume.vue
@@ -1,3 +1,223 @@
 <template>
-	<h1>Resume page</h1>
+	<PageLayout>
+		<PageHeader>Resume</PageHeader>
+		<div class="resume-content">
+		<div class="resume-skills">
+			<h3>Summary</h3>
+			<p>Vast professional experience in web and software programming, database design, integration, management, upgrades, training, and system administration.</p>
+			
+			
+			<h3>Skills</h3>
+			<h4 class="mt-2!">Languages (Advanced)</h4>
+			<p>JavaScript, PHP, CSS, HTML</p>
+			
+			
+			<h4>Languages (I can work on)</h4>
+			<p>Perl, VB, TypeScript, C#, C, C++, Java, Python</p>
+			
+			
+			<h4>Databases</h4>
+			<p>MySQL, SQLite, MongoDB, Custom Database</p>
+			
+			
+			<h4>Technologies &amp; Frameworks</h4>
+			<p>Laravel, PHPUnit, VueJs with Vuex, Nodejs & Express, Laravel Nova, Capacitorjs, Angular 7+, Bootstrap, Tailwind, CodeIgniter, WordPress, Socket.io, jQuery, Gulp, PostCSS, Webpack, Babel, LeafLeat, FFMPEG, GhostScript and Perl CGI.</p>
+			
+			
+			<h4>Tools</h4>
+			<p>Git, Notepad++, NPM, Vite, Composer, Figma, Filezilla, HeidiSQL, PHPMyAdmin, Web Browsers, Apache, IIS, Robo 3T, Atom, Brackets, Visual Studio Code, Visual Studio, Intellij IDEA,  CodeBlocks, MS Office, Adobe Illustrator, Adobe Photoshop, Eclipse.</p>
+			
+			
+			<h4>OS</h4>
+			<p>Windows and Linux.</p>
+        
+        
+        </div>
+        <h3>Work Experience</h3>
+        
+
+        <h4 class="mt-2!">Freelance Programmer (May 2025 – Current)</h4>
+        
+        <h5>Worldwide</h5>
+        
+        <ul>
+            <li>Available for full-time roles.</li>
+            <li>Working on freelance projects while actively seeking full-time opportunities. </li>
+            <li>Currently prototyping a personal productivity tool using Laravel, Vue, and Tailwind. Will be source available.</li>
+        </ul>
+
+        
+        
+        
+
+        <h4>DigitalSitara - Web &amp; Software Programmer (Sep 2021 – May 2025)</h4>
+        
+        <h5>Worldwide</h5>
+        
+        <ul>
+            <li>Started a small business (As a Solo Dev) providing cutting-edge software solutions to clients worldwide.</li>
+            <li>Developed a social media PWA (Progressive Web Application including Android Port with Capacitor) for a community.</li>
+            <li>Created custom WordPress themes from scratch (with ACF) for business websites.</li>
+            <li>Managed Linux servers for multiple projects.</li>
+            <li>Built a real estate portal for a client using Laravel.</li>
+            <li>Designed and developed a dynamic accounting website for a client.</li>
+            <li>Worked as the head programmer to drive company growth.</li>
+            <li>Business operations formally closed in May 2025 to pursue freelancing or full time roles once again.</li>
+        </ul>
+
+        
+        
+        
+        
+        
+        <h4>Freelance Programmer (Dec 2020 – Sep 2021)</h4>
+        
+        <h5>Worldwide</h5>
+        
+        <ul>
+            <li>Worked with Laravel daily on multiple projects.</li>
+            <li>Used Vue and its plugins for frontend development.</li>
+            <li>Worked with Figma to provide UI/UX solutions.</li>
+            <li>Developed custom WordPress themes from scratch based on client requests.</li>
+        </ul>
+
+        
+        
+        
+        
+        <h4>ColorElephant Lda - Fullstack Programmer (Nov 2019 – Nov 2020)</h4>
+        
+        <h5>Porto, Portugal (Remote)</h5>
+        
+        <ul>
+            <li>Developed multiple projects involving REST APIs and complex code.</li>
+            <li>Worked daily with Laravel on various projects.</li>
+            <li>Used Nova and its packages, creating custom solutions with Vue.js and REST APIs.</li>
+            <li>Effectively communicated and participated in team and company-wide meetings.</li>
+        </ul>
+
+        
+        
+        
+        <h4>Freelance Programmer (Oct 2018 – Nov 2019)</h4>
+        
+        <h5>Worldwide</h5>
+        
+        <ul>
+            <li>Worked with the latest JavaScript and PHP technologies, including Angular 7 and Laravel.</li>
+            <li>Developed and integrated REST APIs using Laravel, Passport, and OAuth 2.0.</li>
+            <li>Built a native Android app with Java for a Canadian municipality.</li>
+            <li>Achieved "Top Rated" freelancer status once again on <a href="https://www.upwork.com/freelancers/~01bed55ae9935fc4d2" target="_blank">Upwork</a>.</li>
+        </ul>
+
+        
+        
+        
+        <h4>Costech Technologies Inc - Head/Lead Programmer (April 2015 – Oct 2018)</h4>
+        
+        <h5>Quebec, Canada (Remote)</h5>
+        
+        <ul>
+            <li>Planned software design and architecture, guiding other developers and explaining various programming best practices.</li>
+            <li>Reviewed code from team members, identified mistakes and poor practices, and provided guidance for improvement.</li>
+            <li>Produced high-quality code, created and modified system functions, and conducted thorough testing.</li>
+            <li>Developed critical web software for the company, including modifications, upgrades, deployment, and testing—managed under the supervision of U.S. government agencies such as DPS, DDS, DOR, and DMV.</li>
+            <li>Built a desktop application for the company to update systems with a single click.</li>
+            <li>Developed a chat application using Node.js, MongoDB, and Express, performing testing and necessary upgrades.</li>
+            <li>Created a custom MVC framework in PHP and built a system based on it.</li>
+            <li>Designed a custom file-based CRUD database system using JSON and PHP.</li>
+            <li>Developed small applications using the MEAN stack with Twitter Bootstrap.</li>
+            <li>Conducted deep research on security and encryption, implementing top-tier encryption in software.</li>
+            <li>Ensured the security of PHP web applications by applying best practices and improving overall protection.</li>
+            <li>Diagnosed and resolved software bugs, ensuring issues were properly fixed and tested.</li>
+        </ul>
+
+
+        
+        
+        
+        <h4>Freelance Programmer (April 2013 – April 2015)</h4>
+        
+        <h5>Worldwide</h5>
+        
+        <ul>
+            <li>Collaborated with multiple clients on various web projects, working with individuals from 21 different countries, primarily from the United States.</li>
+            <li>Managed clients and project requirements effectively.</li>
+            <li>Developed diverse projects involving third-party APIs and custom programming.</li>
+            <li>Provided guidance and consultation to clients to achieve the best possible results.</li>
+            <li>Utilized open-source projects to build tailored solutions for clients.</li>
+            <li>Worked on oDesk (now Upwork), achieving "Top Rated" status with a 100% Job Success score.</li>
+        </ul>
+
+
+        
+        
+        
+        <h4>SiliconLeaf Technologies - Programmer/Web Developer (February 2013 – April 2013)</h4>
+        
+        <h5>Surat, India</h5>
+        
+        <ul>
+            <li>Developed new functions and implemented modifications in existing PHP web applications.</li>
+            <li>Addressed security issues and improved the security of existing web applications.</li>
+            <li>Fixed bugs and resolved issues in various applications.</li>
+            <li>Integrated and worked with multiple JavaScript libraries, adding them to applications as needed.</li>
+            <li>Implemented third-party APIs, ensuring proper functionality through thorough testing.</li>
+            <li>Conducted comprehensive testing of web applications to ensure reliability and performance.</li>
+        </ul>
+
+
+        
+        
+        
+        <h4>MalenTek - Programmer/Web Developer (May 2011 – February 2013)</h4>
+        
+        <h5>Berkeley, United States (Remote)</h5>
+        
+        <ul>
+            <li>Designed web applications from start to finish, including software architecture and database design.</li>
+            <li>Developed a custom web application for technical support teams and code management.</li>
+            <li>Managed code, fixed errors, and resolved bugs in web applications.</li>
+            <li>Designed, developed, and managed a desktop application for Windows drivers.</li>
+            <li>Designed and deployed the website for Malentek.</li>
+            <li>Conducted thorough testing for software, web applications, and websites to ensure quality and performance.</li>
+        </ul>
+
+
+        
+        
+        
+        <h4>Charity work - 2016</h4>
+        
+        <h5>Surat, India</h5>
+        
+        <ul>
+            <li>Designed and developed a website/web application, creating a custom MVC framework in PHP specifically for the project.</li>
+            <li>Built a website for a caste in its native language and integrated a third-party API to send OTP codes to mobile phones.</li>
+            <li>Conducted testing and managed the web application while also providing shared server hosting.</li>
+        </ul>
+        
+        
+        
+        <h4>Other Experiments on my own</h4>
+        
+        <ul>
+            <li>Experimented with JSP, Android application development using Java and Android Studio.</li>
+            <li>Explored game development with Unity and Unreal Engine.</li>
+        </ul>
+            
+            
+            
+            
+            
+    </div>
+	</PageLayout>
 </template>
+
+<script lang="ts" setup>
+
+	import PageLayout from '../components/UI/PageLayout.vue';
+	import PageHeader from '../components/UI/PageHeader.vue';
+
+
+</script>
