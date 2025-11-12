85293975e3a9ec4e79f86ab0d3f8ef48fb587b51
parthmp <parthparmaronline@gmail.com>
2025-11-12 09:00:33 +0530

    404 page and favicon

---
+++ app/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+    <p style="font-size: 100px;" class="mt-[1500px]">
+      Error: {{ error.statusCode }}
+	</p>
+    <NuxtLink to="/" class="mt-6 text-3xl text-primary! hover:text-primary-dark! underline!">
+      Go back home
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+// optional: reset error on navigation
+const handleError = () => clearError({ redirect: '/' })
+</script>
